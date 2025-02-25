6e49423efd9662c44599fe7b13163c45a25112ad
Jesus Espinoza Torruco <jespinoza2511@hotmail.com>
2025-02-24 18:43:19 -0600

    commit message

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        const mongoUri = process.env.MONGO_URI
-        console.log(mongoUri)
-        const conn = await mongoose.connect(mongoUri);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
